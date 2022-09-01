c39a6363588812bd4e22f20b0f17a646e4d61f8a
Akram Boktor <akram.boktor2013@gmail.com>
2022-09-01 23:10:17 +0200

    Best time to buy and sell stock Intersection of Two Arrays

---
+++ Best Time to Buy and Sell Stock/Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+ 
+
+Constraints:
+
+1 <= prices.length <= 105
+0 <= prices[i] <= 104
+*/
+
+using System;
+using System.Collections;
+
+namespace Best_ime_to_Buy_and_Sell_Stock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(',', Program.MaxProfit(new int[] { 7, 1, 6, 5 })));
+            Console.WriteLine(string.Join(',', Program.MaxProfit(new int[] { 5, 2, 2, 1 })));
+            Console.WriteLine(string.Join(',', Program.MaxProfit(new int[] { 3, 1, 2, 1 })));
+
+        }
+
+
+        public static int MaxProfit(int[] prices)
+        { // Time executed Limited
+            /*
+            int maxProfit = 0;
+            for (int i = 0; i < prices.Length; i++)
+            {
+                for (int j = i+1; j < prices.Length; j++)
+                {
+                    if(prices[j] > prices[i]&& maxProfit < prices[j] - prices[i])
+                    {
+                        maxProfit = prices[j] - prices[i];
+                    }
+                }
+            }
+            return maxProfit;
+            */
+            int maxProfit = 0;
+            int minValue = prices[0];
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if(prices[i] < minValue)
+                {
+                    minValue = prices[i];
+                }else if (prices[i] - minValue > maxProfit)
+                {
+                    maxProfit = prices[i]-minValue;
+                }
+            }
+
+            return maxProfit;
+        }
+
+    }
+}
---
+++ Intersection of Two Arrays II/Program.cs
@@ -0,0 +1,88 @@
+﻿/*
+ Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+Explanation: [9,4] is also accepted.
+*/
+
+using System;
+using System.Collections;
+
+namespace Intersection_of_Two_ArraysII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(',', Program.Intersect(new int[] { 1, 2, 2, 1 }, new int[] { 2, 2 })));
+            Console.WriteLine(string.Join(',', Program.Intersect(new int[] { 1, 2, 2, 1 }, new int[] { 2 })));
+            Console.WriteLine(string.Join(',', Program.Intersect(new int[] {3,1,2 }, new int[] { 1,1 })));
+            Console.WriteLine(string.Join(',', Program.Intersect(new int[] { 1, 2 }, new int[] {1,1})));
+            Console.WriteLine(string.Join(',',Program.Intersect(new int[] { 4, 9, 5 }, new int[] { 9, 4, 9, 8, 4 })));
+        }
+
+
+        public static int[] Intersect(int[] nums1, int[] nums2)
+        {
+            List<int> intersectArray = new List<int>();
+            if (nums1.Length <= nums2.Length)
+            {
+                foreach (var item in nums1)
+                {
+                    if (nums2.Contains(item))
+                    {
+                        intersectArray.Add(item);
+                        nums2 = nums2.Where((source, index) => index != Array.IndexOf(nums2, item)).ToArray();
+                    }
+                }
+
+            }
+            else
+            {
+                foreach (var item in nums2)
+                {
+                    if (nums1.Contains(item))
+                    {
+                        intersectArray.Add(item);
+                        nums1 = nums1.Where((source, index) => index != Array.IndexOf(nums1, item)).ToArray();
+
+                    }
+                }
+            }
+
+            return intersectArray.ToArray();
+
+            //var numAndCount1 = new Dictionary<int, int>();
+
+            //foreach (var num in nums1)
+            //{
+            //    if (!numAndCount1.ContainsKey(num)) numAndCount1[num] = 0;
+
+            //    numAndCount1[num]++;
+            //}
+
+            //var result = new List<int>();
+
+            //foreach (var num in nums2)
+            //{
+            //    if (numAndCount1.ContainsKey(num) && numAndCount1[num] > 0)
+            //    {
+            //        result.Add(num);
+
+            //        numAndCount1[num]--;
+            //    }
+            //}
+
+            //return result.ToArray();
+        }
+    }
+}
