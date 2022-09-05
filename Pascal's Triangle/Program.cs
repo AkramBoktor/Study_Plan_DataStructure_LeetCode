caeed4f79815a4f2fb2ac0af8b0a39012acc9703
Akram Boktor <akram.boktor2013@gmail.com>
2022-09-05 14:15:33 +0200

    Pascal triangle Resharp matrix

---
+++ Pascal's Triangle/Program.cs
@@ -0,0 +1,51 @@
+﻿
+
+using System.Collections.Generic;
+
+namespace Pascals_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(string.Join(",", Program.Generate(1)));
+            //Console.WriteLine(string.Join(",", Program.Generate(2)));
+            IList<IList<int>> res = Program.Generate(5);
+            foreach (var items in res)
+            {
+                foreach (var item in items)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            Console.WriteLine();
+        }
+
+
+        public static IList<IList<int>> Generate(int numRows)
+        {
+            IList<IList<int>> list = new List<IList<int>>();
+
+         
+            for (int i = 0; i < numRows; i++)
+            {
+                List<int> resevedList = new List<int>();
+                for (int j = 0; j <= i; j++)
+                {
+                    if( j== 0 || j == i)
+                    {
+                        resevedList.Add(1);
+                    }
+                    else
+                    {
+                        resevedList.Add(list[i - 1][j - 1] + list[i - 1][j]);
+
+                    }
+                }
+                list.Add(resevedList);
+            }
+
+            return list;
+        }
+    }
+}
---
+++ Reshape the Matrix/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Reshape_the_Matrix
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Program.MatrixReshape(new int[1, 2, 3 , 3,4, 5 ], 2, 4));
+        }
+
+
+        public static int[][] MatrixReshape(int[][] mat, int r, int c)
+        {
+            int[][] matrixReshape = new int[r][];
+
+            for (int i = 0; i < r; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    matrixReshape[i][j] = mat[i][j];
+                }
+            }
+
+            return matrixReshape;
+        }
+    }
+}
