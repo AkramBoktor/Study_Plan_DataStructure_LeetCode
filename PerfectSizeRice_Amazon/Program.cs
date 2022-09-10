6f42531eac37f56d61816e72b9649c09118f1f09
Akram Boktor <akram.boktor2013@gmail.com>
2022-09-10 13:14:12 +0200

    amazon test

---
+++ PerfectSizeRice_Amazon/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PerfectSizeRice_Amazon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.getMaxFreqDeviation("bbacccabb"));
+        }
+        public static int getMaxFreqDeviation(string s)
+        {
+            if (s.Length == 0)
+            {
+                return 0;
+            }
+            List<int> maxMinValue = new List<int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                int freq = 1;
+                for (int j = 0; j < s.Length; j++)
+                {
+                    if (s[i] == s[j] && i != j)
+                    {
+                        freq += 1;
+                    }
+                }
+                maxMinValue.Add(freq);
+            }
+
+            return maxMinValue.Max() - maxMinValue.Min();
+        }
+
+    }
+
+}
