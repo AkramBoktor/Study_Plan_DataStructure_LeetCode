d4be8c2c56e7e95f90c449bfe97cfd290d6661d8
Akram Boktor <akram.boktor2013@gmail.com>
2022-09-19 13:16:53 +0200

    bitwicw number

---
+++ Bitwice_Numbers/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+
+namespace Bitwice_Numbers
+{
+    internal class Program
+    {
+        //public static int[] sortedXor(int N, int K, int[] A)
+        //{
+        //    if (N == 0 || A.Length == 0) return Array.Empty<int>();
+        //    int[] result = new int[N];
+
+        //    for (int i = 0; i < N; i++)
+        //    {
+        //        result[i] = A[i] ^ K;
+        //    }
+        //    //write your Logic here:
+
+        //    Array.Sort(result);
+        //    return  result;
+        //}
+
+        //public static void Main()
+        //{
+        //    // INPUT [uncomment & modify if required]
+        //    string[] ip = Console.ReadLine().Split(' ');
+        //    int N = int.Parse(ip[0]);
+        //    int K = int.Parse(ip[1]);
+
+        //    int[] A = new int[N];
+        //    string[] tokens = Console.ReadLine().Split();
+        //    int i;
+        //    for (i = 0; i < N; i++)
+        //    {
+        //        A[i] = Convert.ToInt32(tokens[i]);
+        //    }
+
+        //    // OUTPUT [uncomment & modify if required]
+        //    Console.WriteLine(String.Join(",", sortedXor(N, K, A)));
+        //}
+
+
+
+
+        public static int solveThePuzzle(int N, string S)
+        {
+            int count = 1;
+            for (int i = 0; i < N; i++)
+            {
+
+                for (int j = 0; j < N; j++)
+                {
+
+                    int subTwoDigit = j - i;
+                    if (1 <= i && i < j && j <= N && (subTwoDigit == Math.Abs(Convert.ToInt32(S[j])- Convert.ToInt32(S[i]))))
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        public static void Main()
+        {
+            // INPUT [uncomment & modify if required]
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            string S = Console.ReadLine();
+
+            // OUTPUT [uncomment & modify if required]
+            Console.WriteLine(solveThePuzzle(N, S));
+        }
+    }
+}
+
---
+++ Find First and Last Position of Element in Sorted Array/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Find_First_and_Last_Position_of_Element_in_Sorted_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(",", SearchRange(new int[] { 5, 7, 7, 8, 8, 10 }, 8)));
+            Console.WriteLine(String.Join(",", SearchRange(new int[] { 5, 7, 7, 8, 8, 10 }, 6)));
+            Console.WriteLine(String.Join(",", SearchRange(new int[] { }, 0)));
+            Console.WriteLine(String.Join(",", SearchRange(new int[] { 1 }, 1)));
+            Console.WriteLine(String.Join(",", SearchRange(new int[] { 2, 2 }, 2)));
+            Console.WriteLine(String.Join(",", SearchRange(new int[] { 1, 3 }, 1)));
+        }
+
+        public static int[] SearchRange(int[] nums, int target)
+        {
+            if(nums.Length==0) return new int[] { -1,-1 };
+
+            int mid = (nums[0] + nums[nums.Length-1])/2;
+            List<int> result = new List<int>();
+            int position = 0;
+            if(mid < target)
+            {
+                for (position = Array.IndexOf(nums,mid); position < nums.Length; position++)
+                {
+                    if(nums[position] == target) result.Add(position);
+
+                }
+            }
+            else if(mid == target)
+            {
+                for (position = 0; position < nums.Length; position++)
+                {
+                    if (nums[position] == target) result.Add(position);
+
+                }
+            }
+            else
+            {
+                for (position = 0; position <= Array.IndexOf(nums, mid); position++)
+                {
+                    if (nums[position] == target) result.Add(position);
+
+                }
+            }
+           
+            return result.Count == 0 ? new int[] { -1, -1 } : result.Count==1? new int[] { 0, 0 }: result.ToArray();
+        }
+    }
+}
