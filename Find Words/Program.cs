af20193e27f9b9c5b55adbef073d9a49a6d57b3a
Akram Boktor <akram.boktor2013@gmail.com>
2022-10-08 12:07:01 +0200

    To lower Case Finds Words

---
+++ Find Words/Program.cs
@@ -0,0 +1,74 @@
+﻿/*
+ Given an array of strings words, return the words that can be typed using letters of the alphabet on only one row of American keyboard like the image below.
+
+In the American keyboard:
+
+the first row consists of the characters "qwertyuiop",
+the second row consists of the characters "asdfghjkl", and
+the third row consists of the characters "zxcvbnm".
+
+Example 1:
+
+Input: words = ["Hello","Alaska","Dad","Peace"]
+Output: ["Alaska","Dad"]
+Example 2:
+
+Input: words = ["omk"]
+Output: []
+Example 3:
+
+Input: words = ["adsdf","sfd"]
+Output: ["adsdf","sfd"]
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Find_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(String.Join(',', Program.FindWords(new string[] { "Hello", "Alaska", "Dad", "Peace" })));
+            Console.WriteLine(String.Join(',', Program.FindWords(new string[] { "omk" })));
+            Console.WriteLine(String.Join(',', Program.FindWords(new string[] { "adsdf", "sfd" })));
+        }
+
+
+        public static string[] FindWords(string[] words)
+        {
+            List<string> listFindWords = new List<string>();
+            IDictionary<int, string> AmericanKeyboards = new Dictionary<int, string>();
+            int key;
+            int oldKey;
+            AmericanKeyboards.Add(1, "qwertyuiop");
+            AmericanKeyboards.Add(2, "asdfghjkl");
+            AmericanKeyboards.Add(3, "zxcvbnm");
+            for (int i = 0; i < words.Length; i++)
+            {
+                 key = 0;
+                 oldKey = 0;
+                bool theSameRow = true;
+                for (int j = 0; j < words[i].Length; j++)
+                {
+                    oldKey = key;
+
+                    key = AmericanKeyboards.Where(x => x.Value.Contains(words[i][j].ToString().ToLower())).Select(k=>k.Key).FirstOrDefault();
+
+                    if( oldKey !=0 && oldKey != key)
+                    {
+                        theSameRow = false;
+                        break;
+                    }
+                }
+
+                if (theSameRow == true) listFindWords.Add(words[i]);
+            }
+
+
+            return listFindWords.ToArray();
+        }
+    }
+}
---
+++ To Lower Case/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace To_Lower_Case
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Program.ToLowerCase("AAcD"));
+        }
+
+        public static string ToLowerCase(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return string.Empty;
+            StringBuilder lowercaseString = new StringBuilder();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if ((int)s[i] >= 65 && (int)s[i] <= 90)
+                {
+                    lowercaseString.Append((char) ((int)s[i]+32));
+                }
+                else
+                {
+                    lowercaseString.Append(s[i]);
+                }
+            }
+
+            return lowercaseString.ToString();
+        }
+    }
+}
